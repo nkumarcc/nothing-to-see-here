9357bbc7ebcdf652cf77f2010d23e7810ee93350
nkumarcc <nkumarcc@gmail.com>
2023-07-28 17:57:37 -0400

    Added loader and slow type text

--- client/src/components/AskBookForm.tsx
+++ client/src/components/AskBookForm.tsx
@@ -1,10 +1,13 @@
 import React, { useState } from "react";
+import { TypeAnimation } from 'react-type-animation';
+import { GridLoader } from "react-spinners";
 import { Button, Col, Form, Input, Row } from 'reactstrap';
 import './styles/AskBookForm.scss';
 import axios from 'axios';
 
 const AskBookForm: React.FC = () => {
   const [answer, setAnswer] = useState('');
+  const [isAnswering, setIsAnswering] = useState(false);
   const [bookQuestion, setBookQuestion] = useState('');
 
   function handleBookQuestionEdit(event: React.ChangeEvent<HTMLInputElement>) {
@@ -13,10 +16,12 @@ const AskBookForm: React.FC = () => {
 
   const handleAsk = async (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
+    setIsAnswering(true);
     const response = await axios.post('/ask', {
       question: event.currentTarget.bookQuestion.value
     });
     setAnswer(response.data.answer);
+    setIsAnswering(false);
   }
 
   const handleFeelingLucky = async (event: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
@@ -28,10 +33,12 @@ const AskBookForm: React.FC = () => {
     ],
     random = ~~(Math.random() * options.length);
     setBookQuestion(options[random]);
+    setIsAnswering(true);
     const response = await axios.post('/ask', {
       question: options[random]
     });
     setAnswer(response.data.answer);
+    setIsAnswering(false);
   }
 
   const handleAnotherQuestion = (event: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
@@ -45,7 +52,7 @@ const AskBookForm: React.FC = () => {
         <div className="text-start">
           <Row className="mt-3">
             <Col>
-              <p>{answer}</p>
+              <TypeAnimation sequence={[answer]} wrapper="span" />
             </Col>
           </Row>
           <Row className="mt-3">
@@ -57,12 +64,25 @@ const AskBookForm: React.FC = () => {
       )
     } else {
       return (
-        <Row className="mt-3">
-          <Col>
-            <Button type="submit" className="me-1">Ask Question</Button>
-            <Button type="button" className="ms-1" onClick={handleFeelingLucky}>I'm Feeling Lucky</Button>
-          </Col>
-        </Row>
+        <>
+          <Row className="mt-3">
+            <Col>
+              <GridLoader
+                loading={isAnswering}
+                size={8}
+                aria-label="Loading Spinner"
+                className="mx-auto"
+                data-testid="loader"
+              />
+            </Col>
+          </Row>
+          <Row className="mt-3">
+            <Col>
+              <Button type="submit" className="me-1">Ask Question</Button>
+              <Button type="button" className="ms-1" onClick={handleFeelingLucky}>I'm Feeling Lucky</Button>
+            </Col>
+          </Row>
+        </>
       )
     }
   }
