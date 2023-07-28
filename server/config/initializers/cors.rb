4b66f05cf6b7c72575586fd898d5d1418eadde52
nkumarcc <nkumarcc@gmail.com>
2023-07-28 14:44:37 -0400

    Updated CORS

--- server/config/initializers/cors.rb
+++ server/config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'localhost:4200'
+    origins 'localhost:4200', 'https://main--comfy-kleicha-229087.netlify.app/'
     resource '*',
       headers: :any,
       methods: [:get, :post, :put, :patch, :delete, :options, :head]
