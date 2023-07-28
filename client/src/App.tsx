5a7b7fc95a44252bf70aecfbcbd93a139b48774d
nkumarcc <nkumarcc@gmail.com>
2023-07-28 15:28:30 -0400

    Add book and tinkered with book

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,16 @@
 import React from 'react';
-import './App.css';
+import './App.scss';
 import AskBookForm from './components/AskBookForm';
-import { Container } from 'reactstrap';
+import { Col, Container, Row } from 'reactstrap';
 
 function App() {
   return (
     <Container className="App mt-5">
+      <Row>
+        <Col>
+          <img src={process.env.PUBLIC_URL + '/book.png'} alt="Book" />
+        </Col>
+      </Row>
       <h1>Ask My Book</h1>
       <p>This is my product engineering challenge to prove that I am indeed worthy.</p>
       <AskBookForm />
